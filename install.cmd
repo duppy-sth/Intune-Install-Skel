fb8a4c802016cd0395130c8de29ad50d8736b587
duppy <75955117+duppy-sth@users.noreply.github.com>
2024-01-31 08:53:41 +0000

    Add files via upload

---
+++ install.cmd
@@ -0,0 +1,3 @@
+xcopy "Source Folder" "c:\Program Files (x86)\Destination Folder" /i /e /y
+
+copy "Sortcut.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Shortcut.lnk" /y
---
+++ uninstall.cmd
@@ -0,0 +1,2 @@
+rmdir "C:\Program Files (x86)\Folder to delete" /Q /S
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Shortcut.lnk" /Q
